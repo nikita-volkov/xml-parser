ca8e78d074eec3e32c138a3be67374dddf81932e
Nikita Volkov <nikita.y.volkov@mail.ru>
2021-06-07 21:01:30 +0300

    Implement most of it

--- library/XmlTypesParser.hs
+++ library/XmlTypesParser.hs
@@ -1,11 +1,29 @@
 module XmlTypesParser
-  (
+  ( -- * Execution
+    parseElement,
+    Error (..),
+    Location (..),
+
+    -- * Parsers by context
+
+    -- ** Element
+    Element,
+    childrenByName,
+    attributesByName,
+
+    -- ** ByName
+    ByName,
+    byName,
   )
 where
 
 import qualified Data.Attoparsec.Text as Atto
+import qualified Data.HashMap.Strict as HashMap
 import qualified Data.XML.Types as Xml
 import XmlTypesParser.Prelude
+
+parseElement :: Element a -> Xml.Element -> Either Error a
+parseElement (Element run) = run
 
 data Error = Error [Location] Reason
 
@@ -24,7 +42,11 @@ newtype Element a
 -- Look up the first element by name and parse it.
 childrenByName :: ByName Element a -> Element a
 childrenByName =
-  error "TODO"
+  \(ByName runByName) -> Element $ \element ->
+    let lookup = buildByNameLookup (nodesToElementsByName (Xml.elementNodes element))
+     in runByName lookup exec
+  where
+    exec element (Element run) = run element
 
 -- |
 -- Look up the first attribute by name and parse it.
@@ -96,3 +118,35 @@ byName ns name parser =
       Right a -> Right a
       Left (Error path reason) -> Left (Error (ByNameLocation ns name : path) reason)
     Nothing -> Left (Error [] (NameNotFoundReason ns name))
+
+{-# INLINE buildByNameLookup #-}
+buildByNameLookup :: Foldable f => f (Xml.Name, a) -> Maybe Text -> Text -> Maybe a
+buildByNameLookup list =
+  foldr step lookup list HashMap.empty HashMap.empty
+  where
+    step (Xml.Name name ns _, contents) next !map1 !map2 =
+      case ns of
+        Just ns ->
+          next
+            ( HashMap.alter
+                ( maybe
+                    (Just (HashMap.singleton name contents))
+                    (Just . HashMap.insert name contents)
+                )
+                ns
+                map1
+            )
+            map2
+        Nothing ->
+          next map1 (HashMap.insert name contents map2)
+    lookup map1 map2 ns name =
+      case ns of
+        Just ns -> HashMap.lookup ns map1 >>= HashMap.lookup name
+        Nothing -> HashMap.lookup name map2
+
+{-# INLINE nodesToElementsByName #-}
+nodesToElementsByName :: [Xml.Node] -> [(Xml.Name, Xml.Element)]
+nodesToElementsByName =
+  mapMaybe $ \case
+    Xml.NodeElement element -> Just (Xml.elementName element, element)
+    _ -> Nothing
