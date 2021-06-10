2fad2cdd73b7523963550c17336c16b91149ff25
Nikita Volkov <nikita.y.volkov@mail.ru>
2021-06-10 13:34:49 +0300

    Integrate with xml-conduit

--- library/XmlUnscrambler.hs
+++ library/XmlUnscrambler.hs
@@ -23,18 +23,41 @@ module XmlUnscrambler
   )
 where
 
-import qualified Text.XML as Xml
+import qualified Data.ByteString.Lazy as LazyByteString
+import qualified Data.Text as Text
+import qualified Text.XML as XmlConduit
+import qualified Text.XML.Unresolved as XmlConduit (InvalidEventStream (..))
 import qualified XmlUnscrambler.AstParser as AstParser
 import XmlUnscrambler.Prelude
 
+{-|
+Parse the XML document bytestring.
+-}
 parseByteString :: AstParser.Element a -> ByteString -> Either Text a
-parseByteString astParser input =
-  error "TODO"
+parseByteString astParser =
+  parseLazyByteString astParser . LazyByteString.fromStrict
+
+parseLazyByteString :: AstParser.Element a -> LazyByteString.ByteString -> Either Text a
+parseLazyByteString astParser input =
+  first renderFirstStageError (XmlConduit.parseLBS xmlConduitSettings input)
+    >>= first renderSecondStageError . AstParser.parseElement astParser . XmlConduit.documentRoot
+  where
+    xmlConduitSettings =
+      def
+        { XmlConduit.psRetainNamespaces = True
+        }
 
 renderFirstStageError :: SomeException -> Text
-renderFirstStageError =
-  -- FIXME: Do something more user-friendly
-  fromString . show
+renderFirstStageError e
+  | Just e <- fromException @XmlConduit.XMLException e =
+    fromString (show e)
+  | Just e <- fromException @XmlConduit.InvalidEventStream e =
+    fromString (show e)
+  | Just (XmlConduit.UnresolvedEntityException e) <- fromException @XmlConduit.UnresolvedEntityException e =
+    "Unresolved entities: " <> Text.intercalate "," (toList e)
+  | otherwise =
+    -- FIXME: Find other cases and do something more user-friendly about them
+    fromString (show e)
 
 renderSecondStageError :: AstParser.Error -> Text
 renderSecondStageError =
--- library/XmlUnscrambler/Prelude.hs
+++ library/XmlUnscrambler/Prelude.hs
@@ -28,6 +28,7 @@ import Data.Char as Exports
 import Data.Coerce as Exports
 import Data.Complex as Exports
 import Data.Data as Exports
+import Data.Default.Class as Exports (Default (..))
 import Data.Dynamic as Exports
 import Data.Either as Exports
 import Data.Fixed as Exports
